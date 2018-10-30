1f9afd6fad3c31e491eb434f65a999bf4f0f4427
Cameron Roberts <example@gmail.com>
2018-10-30 09:48:13 -0700

    Modified app.js adding CORS and SkierTerms

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 var express = require("express");
-
+var cors = require("cors");
 var app = express();
+
+var skierTerms = [
+    {
+        term: "Rip",
+        defined: "To move at a high rate of speed"
+    },
+    {
+        term: "Huck",
+        defined: "To throw your body off of something, usually a natural feature like a cliff"        
+    },
+    {
+        term: "Chowder",
+        defined: "Powder after it has been sufficiently skied"
+    }
+];
+
+
+app.use(function(req, res, next) {
+    
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+    
+});
 
 app.use(express.static("./public"));
 
+app.use(cors());
 
-app.listen(3000);
+app.get("/dictionary-api", function(req, res) {
+    res.json(skierTerms);
+});
 
-console.log("Express app running on port 3000");
+
+app.listen(3001);
+
+console.log("Express app running on port 3001 > 'localhost:3001'");
 
 module.exports = app;
